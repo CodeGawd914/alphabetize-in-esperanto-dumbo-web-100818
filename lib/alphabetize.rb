85d4fb96cf32b988aa60cbafaf4eddb162d3c358
Travis Desir <travisgdesir31@gmail.com>
2018-09-27 04:50:55 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,7 @@
 def alphabetize(arr)
   expo = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do(thing)
-  thing.map do |letters|
+  thing.split("").collect do |letters|
   expo.index(letters)
 end
 end
