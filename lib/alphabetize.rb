7ebd75a09b23a10aa6c1f5f2c309edb903420782
Travis Desir <travisgdesir31@gmail.com>
2018-09-27 04:55:59 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,8 +1,8 @@
-def alphabetize(arr)
-  expo = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  arr.sort_by do(thing)
-  thing.split("").collect do |letters|
-  expo.index(letters)
-end
-end
-end
+# def alphabetize(arr)
+#   expo = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+#   arr.sort_by do(thing)
+#   thing.split("").collect do |letters|
+#   expo.index(letters)
+# end
+# end
+# end
