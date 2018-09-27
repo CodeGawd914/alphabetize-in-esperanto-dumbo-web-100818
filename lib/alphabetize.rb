2ecbf7eb44f6c570c77e3dae9fa15e8143f70b5d
Travis Desir <travisgdesir31@gmail.com>
2018-09-27 04:56:24 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -9,6 +9,7 @@
 
 
 def alphabetize(sentences_array)
+  ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   sentences_array.sort_by do |sentence|
     sentence.split("").map do |character|
       ESPERANTO_ALPHABET.index(character)
