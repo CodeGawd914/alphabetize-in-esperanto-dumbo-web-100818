17a550266db93f8ee1b0f99c2fac4e80e36c468f
Travis Desir <travisgdesir31@gmail.com>
2018-09-27 04:56:03 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -6,3 +6,12 @@
 # end
 # end
 # end
+
+
+def alphabetize(sentences_array)
+  sentences_array.sort_by do |sentence|
+    sentence.split("").map do |character|
+      ESPERANTO_ALPHABET.index(character)
+    end
+  end
+end
